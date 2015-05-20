a46de3d5c0a40d5b0d889b6962d5b731ea089489
Mogens Heller Grabe <mookid8000@gmail.com>
2015-05-20 23:54:51 +0200

    added full build script

--- publish.bat
+++ publish.bat
@@ -1,1 +1,11 @@
 msbuild Injectionist\Injectionist.csproj /p:Configuration=Release
+
+if exist deploy (
+  rd deploy /s/q
+)
+
+if not exist deploy (
+  mkdir deploy
+)
+
+tools\nuget\nuget pack Injectionist\Injectionist.nuspec -OutputDirectory deploy
